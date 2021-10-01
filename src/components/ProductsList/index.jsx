2996eb03f2998028bbb2a141f222e0285f9aed68
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-09-30 21:46:00 -0300

    Fixing what was asked

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -6,8 +6,8 @@ const ProductsList = ({ products, addToCart }) => {
           <li key={key}>
             <h3>{item.name}</h3>
             <p>{item.description}</p>
-            <p>Preço Original: R$ {item.price.toFixed(2)}</p>
-            <p>Promoção: R$ {(item.price - item.discount).toFixed(2)}</p>
+            <p>Preço Original: R$ {Number(item.price).toFixed(2)}</p>
+            <p>Promoção: R$ {Number(item.price - item.discount).toFixed(2)}</p>
             <button onClick={() => addToCart(item.code)}>Adicionar</button>
           </li>
         );
